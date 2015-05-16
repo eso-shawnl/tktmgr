23ce12b614b7a9eb296a084c95be52c3805fc1fe
Shawn Lin <jallen0927@hotmail.com>
2015-05-16 12:36:49 +1200

    first commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/xlin/Sites/opencart/admin/');
+define('DIR_SYSTEM', '/Users/xlin/Sites/opencart/system/');
+define('DIR_LANGUAGE', '/Users/xlin/Sites/opencart/admin/language/');
+define('DIR_TEMPLATE', '/Users/xlin/Sites/opencart/admin/view/template/');
+define('DIR_CONFIG', '/Users/xlin/Sites/opencart/system/config/');
+define('DIR_IMAGE', '/Users/xlin/Sites/opencart/image/');
+define('DIR_CACHE', '/Users/xlin/Sites/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/Users/xlin/Sites/opencart/system/download/');
+define('DIR_UPLOAD', '/Users/xlin/Sites/opencart/system/upload/');
+define('DIR_LOGS', '/Users/xlin/Sites/opencart/system/logs/');
+define('DIR_MODIFICATION', '/Users/xlin/Sites/opencart/system/modification/');
+define('DIR_CATALOG', '/Users/xlin/Sites/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', '3adb');
+define('DB_PASSWORD', 'Feb232015');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
